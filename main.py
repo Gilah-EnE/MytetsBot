8a58473a3108b6e3f60c5f57fd43404046f0bcc0
Artem <63448341+Gilah-EnE@users.noreply.github.com>
2022-08-22 18:29:50 +0300

    Перероблено структуру

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import hashlib
-import aiosqlite
+import re
+import sqlite3
 import aiogram
-import aiogram.types
 import json
 import logging
 import random
 import string
+from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,31 +15,57 @@ with open("config.json", "r") as conf_file:
 bot = aiogram.Bot(token=conf['api']['token'])
 dp = aiogram.Dispatcher(bot)
 
-@dp.inline_handler()
+
+def get_all_phrases() -> dict:
+    rows = dict()
+    try:
+        connection = sqlite3.connect("phrases.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM phrases")
+        rows = cursor.fetchall()
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    return rows
+
+
+def random_phrase(phrases: dict):
+    return random.choice(phrases)
+
+
+@dp.inline_handler(lambda query: len(query.query) == 0)
 async def inline_echo(inline_query: aiogram.types.InlineQuery):
-    # id affects both preview and content,
-    # so it has to be unique for each result
-    # (Unique identifier for this result, 1-64 Bytes)
-    # you can set your unique id's
-    # but for example i'll generate it based on text because I know, that
-    # only text will be passed in this example
-
-    rows = list()
-    async with aiosqlite.connect("phrases.db") as db:   
-        async with db.execute("SELECT * FROM phrases") as cursor:
-            async for row in cursor:
-                rows.append(row)
-
-    text = inline_query.query or 'echo'
-    input_content = aiogram.types.InputTextMessageContent(random.choice(rows)[1], parse_mode=aiogram.types.ParseMode.HTML)
-    result_id: str = hashlib.md5(''.join(random.choice(string.ascii_letters) for i in range(24)).encode()).hexdigest()
-    item = aiogram.types.InlineQueryResultArticle(
-        id=result_id,
-        title=f'Result {text!r}',
-        input_message_content=input_content,
+    i1 = aiogram.types.InlineQueryResultArticle(
+        id=1,
+        title="Випадкова цитата",
+        description="Цитат в боті, як масла, тобто, дохуя!",
+        input_message_content=aiogram.types.InputTextMessageContent(random_phrase(get_all_phrases())[1],
+                                                                    parse_mode=aiogram.types.ParseMode.HTML)
     )
-    # don't forget to set cache_time=1 for testing (default is 300s or 5m)
-    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
+    i2 = aiogram.types.InlineQueryResultArticle(
+        id=2,
+        title="Пошук цитати",
+        description="Для пошуку введіть уривок, назву твору або ключові слова",
+        input_message_content=aiogram.types.InputTextMessageContent("Test")
+    )
+
+    await bot.answer_inline_query(inline_query.id, results=[i1, i2])
+
+
+@dp.inline_handler(lambda query: len(query.query) > 0)
+async def search_echo(inline_query: aiogram.types.InlineQuery):
+    articles = list()
+    subs = inline_query.query or 'random'
+    found_phrases = [_ for _ in get_all_phrases() if re.search(subs, _[1])]
+    for phrase in found_phrases:
+        soup = BeautifulSoup(phrase[1], features="html.parser")
+        articles.append(
+            aiogram.types.InlineQueryResultArticle(id=phrase[0], title=soup.i.string, description=soup.b.string,
+                                                   input_message_content=aiogram.types.InputTextMessageContent(
+                                                       phrase[1], parse_mode=aiogram.types.ParseMode.HTML)))
+
+    await bot.answer_inline_query(inline_query.id, results=articles)
 
 
 if __name__ == '__main__':
